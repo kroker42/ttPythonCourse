f96ba2003b163f44528eb802c1ef99cafb214687
kroker42 <git@frisch.se>
2022-04-05 14:14:44 +0100

    Initial setup

---
+++ reduce.py
@@ -0,0 +1,23 @@
+# reduce is an implementation of the mathematical concept 'fold' or 'reduction'.
+
+# It is often used in functional programming.
+# Functional programming uses a series of **pure functions** to work on **immutable data**.
+# The functions do not rely on **internal state**.
+# Functional programming favours **recursion** over e.g. loops.
+
+# Python is a multi-paradigm language that supports both object-oriented programming and functional programming.
+# The programming style is different from OOP and functional programming.
+# The most important principle of The Pythonic Way is:
+#
+#           Simple is better than complex
+#
+
+# We will explore different ways of implementing reduce in python.
+
+# Reduce uses another function to fold a collection of items together into a single, cumulative value.
+# I.e. the input to reduce is another function and many items, and the output is a single item.
+
+# In python, functions are **first-class objects**.
+
+def reduce():
+    return False
---
+++ reduce_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class ReduceTestCase(unittest.TestCase):
+    def test_reduce(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
